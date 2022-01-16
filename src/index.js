baa8a63ed2e6d6882ee5bd542180fee22f76b2d6
Vinicius Augusto <vinicius.pantro@hotmail.com>
2022-01-16 13:03:59 -0300

    mapadev-week-2022

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const imagensPainel = document.querySelectorAll(".imagem-painel");
+const setaAvancar = document.getElementById("btn-avancar");
+const setaVoltar = document.getElementById("btn-voltar");
+let imagemAtual = 0;
+
+function esconderImagens() {
+  imagensPainel.forEach((imagem) => {
+    imagem.classList.remove("mostrar");
+  });
+}
+function mostrarImagens() {
+  imagensPainel[imagemAtual].classList.add("mostrar");
+}
+setaAvancar.addEventListener("click", function () {
+  const totalDeImagne = imagensPainel.length - 1;
+  if (imagemAtual === totalDeImagne) {
+    return;
+  }
+  imagemAtual++;
+  esconderImagens();
+  mostrarImagens();
+});
+setaVoltar.addEventListener("click", function () {
+  if (imagemAtual === 0) {
+    return;
+  }
+  imagemAtual--;
+  esconderImagens();
+  mostrarImagens();
+});
